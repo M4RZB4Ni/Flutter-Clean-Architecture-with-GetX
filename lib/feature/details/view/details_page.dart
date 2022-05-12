985f6ae479ffbe9a3a47885dfc696a1b22880871
Marzbani <hamied.reza.m@gmail.com>
2022-05-12 10:27:33 -0700

    [FEA] download percentage is done

--- lib/feature/details/view/details_page.dart
+++ lib/feature/details/view/details_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:wallpaper_application_assessment/app/base/base_view.dart';
 import 'package:wallpaper_application_assessment/app/resources/app_size.dart';
-import 'package:wallpaper_application_assessment/app/resources/app_text_style.dart';
 import 'package:wallpaper_application_assessment/feature/details/components/app_details_footer.dart';
 import 'package:wallpaper_application_assessment/feature/details/domain/controller/details_page_controller.dart';
 import 'package:wallpaper_application_assessment/feature/general_components/app_button_active.dart';
