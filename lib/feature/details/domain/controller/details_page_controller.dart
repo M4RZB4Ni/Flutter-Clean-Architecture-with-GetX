5020936659146d3de6b37e2387186b52dceec2cd
Marzbani <hamied.reza.m@gmail.com>
2022-05-12 17:10:38 +0430

    [FEA] start implement download image

--- lib/feature/details/domain/controller/details_page_controller.dart
+++ lib/feature/details/domain/controller/details_page_controller.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:image_downloader/image_downloader.dart';
 import 'package:wallpaper_application_assessment/app/base/base_controller.dart';
+import 'package:wallpaper_application_assessment/app/messages/printers.dart';
 import 'package:wallpaper_application_assessment/data/data_models/wallpapaers/response/get_wallpaper_response.dart';
 
 class DetailsPageController extends BaseController{
@@ -10,6 +13,25 @@ class DetailsPageController extends BaseController{
   void onInit() {
     wallpaper=Get.arguments;
     super.onInit();
+  }
+
+  void downloadImage({required final String imagePath}) async
+  {
+    try {
+      // Saved with this method.
+      final imageId = await ImageDownloader.downloadImage(imagePath);
+      if (imageId == null) {
+        return;
+      }
+
+      final fileName = await ImageDownloader.findName(imageId);
+      final path = await ImageDownloader.findPath(imageId);
+      final size = await ImageDownloader.findByteSize(imageId);
+      final mimeType = await ImageDownloader.findMimeType(imageId);
+
+    } on PlatformException catch (error) {
+      realDebugPrint(error);
+    }
   }
 
 }
