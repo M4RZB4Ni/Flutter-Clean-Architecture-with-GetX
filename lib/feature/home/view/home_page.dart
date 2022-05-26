60a932115ed797d21233943585aae7a22e563b81
Marzbani <hamied.reza.m@gmail.com>
2022-05-26 02:26:05 -0700

    [REF] onWillPop modified

--- lib/feature/home/view/home_page.dart
+++ lib/feature/home/view/home_page.dart
@@ -7,6 +7,12 @@ import 'package:wallpaper_application_assessment/feature/home/components/app_wal
 import 'package:wallpaper_application_assessment/feature/home/domain/controller/home_controller.dart';
 
 class HomePage extends BaseView<HomePageController>{
+
+  @override
+  Future<bool> onWillPop(final BuildContext context) async {
+    return false;
+  }
+
   @override
   Widget body(final BuildContext context) {
     return APPWallpaperListView(key: const Key('homeListView'),wallpapers: controller.wallpapers, imageSizes: [Get.width,AppSize.s300],onTap: controller.goToDetailsPage,);
